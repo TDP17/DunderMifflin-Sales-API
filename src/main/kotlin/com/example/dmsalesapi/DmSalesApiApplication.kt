5c4ac3236606154108010a18cfe3c3b771c5396b
OjasInamdar <int.ojas.inamdar@clodura.ai>
2022-08-28 19:22:12 +0530

    [setup]: Initial setup

---
+++ src/main/kotlin/com/example/dmsalesapi/DmSalesApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dmsalesapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DmSalesApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DmSalesApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/dmsalesapi/DmSalesApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dmsalesapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DmSalesApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
