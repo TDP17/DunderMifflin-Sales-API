28f95d8fbbc950cd9d91a2852eea9e64d7e9b266
OjasInamdar <int.ojas.inamdar@clodura.ai>
2022-09-05 14:32:54 +0530

    [test]: Update route tests

--- src/test/kotlin/com/example/dmsalesapi/service/ItemServiceTest.kt
+++ src/test/kotlin/com/example/dmsalesapi/service/ItemServiceTest.kt
@@ -3,6 +3,8 @@ package com.example.dmsalesapi.service
 import com.example.dmsalesapi.model.Item
 import com.example.dmsalesapi.repository.ItemRepository
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -17,97 +19,120 @@ import org.springframework.test.web.servlet.post
 internal class ItemServiceTest @Autowired constructor(
     val mockMvc: MockMvc, val objectMapper: ObjectMapper, val itemRepository: ItemRepository
 ) {
+
     val baseURL: String = "/items/"
 
-    @Test
-    fun `should throw an error if item name is duplicate`() {
-        //before
+    @Nested
+    @DisplayName("Post Tests")
 
-        // For some reason if id is any duplicate value, no exception will be thrown but no data will be added either
-        // This leads to the below workaround
-        // @TODO FIX LATER
-        val item = Item(id = null, name = "TestName", quantity_available = 10, price = 10)
+    inner class PostRouteTests {
+        @Test
+        fun `should throw an error if item name is duplicate`() {
+            // For some reason if id is any duplicate value, no exception will be thrown but no data will be added either
+            // This leads to the below workaround
+            // @TODO FIX LATER
+            val item = Item(id = null, name = "TestName", quantity_available = 10, price = 10)
 
-        //when
-        itemRepository.deleteByName(item.name)
+            //when
+            itemRepository.deleteByName(item.name)
 
-        val tempItem = itemRepository.save(item)
-        item.id = tempItem.id?.plus(1)
-        val performPost = mockMvc.post(baseURL) {
-            contentType = MediaType.APPLICATION_JSON
-            content = objectMapper.writeValueAsString(item)
-        }
-
-        //then
-        performPost.andDo { print() }.andExpect {
-            status { isBadRequest() }
-            content {
-                contentType(MediaType.APPLICATION_JSON)
-                json("{\"status\":400,\"message\":\"Another paper with the same name exists, please update the entity instead\"}")
+            val tempItem = itemRepository.save(item)
+            item.id = tempItem.id?.plus(1)
+            val performPost = mockMvc.post(baseURL) {
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(item)
             }
+
+            //then
+            performPost.andDo { print() }.andExpect {
+                status { isBadRequest() }
+                content {
+                    contentType(MediaType.APPLICATION_JSON)
+                    json("{\"status\":400,\"message\":\"Another paper with the same name exists, please update the entity instead\"}")
+                }
+            }
+
+            // after
+            itemRepository.deleteByName(item.name)
         }
 
-        // after
-        itemRepository.deleteByName(item.name)
-    }
+        @Test
+        fun `should add the new item if name is not duplicate`() {
+            //before
+            val price = 2000
+            val quantity_available = 2000
+            itemRepository.deleteByName("NAMEWHICHWILLNEVERBECREATED")
 
+            // given
+            val newItem = Item(
+                id = null, name = "NAMEWHICHWILLNEVERBECREATED", price = price, quantity_available = quantity_available
+            )
 
-    @Test
-    fun `should add the new item if name is not duplicate`() {
-        //before
-        val price: Int = 2000
-        val quantity_available: Int = 2000
-        itemRepository.deleteByName("NAMEWHICHWILLNEVERBECREATED")
+            // when
+            val performPost = mockMvc.post(baseURL) {
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(newItem)
+            }
 
-        // given
-        val newItem = Item(id = null, name = "NAMEWHICHWILLNEVERBECREATED", price = price, quantity_available = quantity_available)
-
-        // when
-        val performPost = mockMvc.post(baseURL) {
-            contentType = MediaType.APPLICATION_JSON
-            content = objectMapper.writeValueAsString(newItem)
-        }
-
-        // then
-        performPost
-            .andDo { print() }
-            .andExpect {
+            // then
+            performPost.andDo { print() }.andExpect {
                 status { isCreated() }
                 content {
                     contentType(MediaType.APPLICATION_JSON)
                 }
             }
 
-        // after
-        itemRepository.deleteByName("NAMEWHICHWILLNEVERBECREATED");
+            // after
+            itemRepository.deleteByName("NAMEWHICHWILLNEVERBECREATED")
+        }
     }
 
-    @Test
-    fun `should update item and return Accepted (204)`() {
-        //before
-        val item = Item(id = null, name = "UpdateName", quantity_available = 10, price = 10)
-        itemRepository.save(item);
+    @Nested
+    @DisplayName("Update Tests")
+    inner class UpdateRouteTests {
+        @Test
+        fun `should return 404 and appropriate error if given id is not found`() {
+            // given
+            val item = Item(id = -1, name = "TestName", quantity_available = 10, price = 10)
 
-        val newItem = Item(id = null, name = "NewUpdatedItem", quantity_available = 20, price = 20)
-
-        // when
-        val performPost = mockMvc.patch("$baseURL/${item.id}") {
-            contentType = MediaType.APPLICATION_JSON
-            content = objectMapper.writeValueAsString(newItem)
-        }
-
-        // then
-        performPost
-            .andDo { print() }
-            .andExpect {
-                status { isNoContent() }
-                content {
-                    contentType(MediaType.APPLICATION_JSON)
-                    json("{\"status\":204,\"message\":\"Updated successfully\"}")
-                }
+            // when
+            val performPost = mockMvc.patch("$baseURL/-1") {
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(item)
             }
 
-        // after
-        itemRepository.delete(item);
+            // then
+            performPost.andDo { print() }.andExpect {
+                status { isNotFound() }
+                content {
+                    json("{\"status\":404,\"message\":\"No item with given details found\"}")
+                }
+            }
+        }
+
+        @Test
+        fun `should update item and return Accepted (204) if request is correct`() {
+            // before
+            val item = Item(id = null, name = "UpdateName", quantity_available = 10, price = 10)
+            itemRepository.save(item)
+
+            // given
+            val newItem = Item(id = null, name = "NewUpdatedItem", quantity_available = 20, price = 20)
+
+            // when
+            val performPost = mockMvc.patch("$baseURL/${item.id}") {
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(newItem)
+            }
+
+            // then
+            performPost.andDo { print() }.andExpect {
+                status { isNoContent() }
+            }
+
+            // after
+            itemRepository.delete(item)
+            itemRepository.delete(newItem)
+        }
     }
 }
