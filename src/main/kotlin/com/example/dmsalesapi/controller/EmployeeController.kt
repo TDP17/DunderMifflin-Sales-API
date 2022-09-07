1bb814757dc74839e987fbaf351c0b97058088f8
OjasInamdar <int.ojas.inamdar@clodura.ai>
2022-09-07 11:42:31 +0530

    [feat]: Get employee routes

--- src/main/kotlin/com/example/dmsalesapi/controller/EmployeeController.kt
+++ src/main/kotlin/com/example/dmsalesapi/controller/EmployeeController.kt
@@ -1,19 +1,22 @@
 package com.example.dmsalesapi.controller
 
 import com.example.dmsalesapi.model.Employee
-import com.example.dmsalesapi.model.Item
 import com.example.dmsalesapi.service.EmployeeService
 import com.fasterxml.jackson.databind.JsonNode
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping
 class EmployeeController(private val employeeService: EmployeeService) {
+    @GetMapping("/employees")
+    fun getEmployees(): MutableIterable<Employee> = employeeService.getEmployees()
+
+    @GetMapping("/employees/{id}")
+    fun getEmployeeById(@PathVariable id: Int): Optional<Employee> = employeeService.getEmployeeById(id)
+
     @PostMapping("/employees")
     fun createEmployee(@RequestBody data: JsonNode): ResponseEntity<Employee> {
         val newEmployee: Employee = employeeService.createEmployee(data)
--- src/main/kotlin/com/example/dmsalesapi/service/EmployeeService.kt
+++ src/main/kotlin/com/example/dmsalesapi/service/EmployeeService.kt
@@ -4,9 +4,14 @@ import com.example.dmsalesapi.model.Employee
 import com.example.dmsalesapi.repository.EmployeeRepository
 import com.fasterxml.jackson.databind.JsonNode
 import org.springframework.stereotype.Service
+import java.util.Optional
 
 @Service
 class EmployeeService(private val employeeRepository: EmployeeRepository) {
+    fun getEmployees(): MutableIterable<Employee> = employeeRepository.findAll()
+
+    fun getEmployeeById(id: Int): Optional<Employee> = employeeRepository.findById(id)
+
     fun createEmployee(data: JsonNode): Employee {
         val employee = Employee(
             id = null,
@@ -15,6 +20,7 @@ class EmployeeService(private val employeeRepository: EmployeeRepository) {
         )
 
         return try {
+            // @TODO consider a trigger for this instead of 2 operations and their error handling
             val createdEmployee = employeeRepository.save(employee)
             employeeRepository.insertIntoHrTable(true, employee.id!!)
             createdEmployee
